4c95384d05a4ed438ad0def57a805439445aba6e
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-09-02 15:54:24 +0200

    Add a test

--- packages/e/enginesquared/xmake.lua
+++ packages/e/enginesquared/xmake.lua
@@ -7,6 +7,7 @@ package("enginesquared")
 
     add_urls("https://github.com/EngineSquared/EngineSquared.git")
 
+    add_versions("v0.1.0", "c93dc690c80147a9c208cc5cd921335b61ed53a6")
     add_versions("webgpu", "7cd0ecfb587e87939e1481f508cf8941cc85b2d8")
 
     on_install(function (package)
@@ -19,5 +20,5 @@ package("enginesquared")
 
     on_test(function (package)
         -- TODO check includes and interfaces
-        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+        assert(package:has_cxxtypes("Core", {includes = "Core.h"})
     end)
