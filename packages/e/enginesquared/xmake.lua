08de32267caec0c92e53c53a8e5f5cce24d05cbf
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-09-03 11:14:31 +0200

    Update version

--- packages/e/enginesquared/xmake.lua
+++ packages/e/enginesquared/xmake.lua
@@ -8,7 +8,7 @@ package("enginesquared")
     add_urls("https://github.com/EngineSquared/EngineSquared.git")
 
     add_versions("v0.1.0", "c93dc690c80147a9c208cc5cd921335b61ed53a6")
-    add_versions("webgpu", "8c5363a6c57109e3e549d96e9a5d6d5fbf51ca93")
+    add_versions("webgpu", "2d4691c85b6e163ffb911f20fd8f917c7d442ef2")
 
     on_install(function (package)
         local configs = {}
